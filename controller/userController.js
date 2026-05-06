6542e1e2b7d284da852a417828c5f6c1ed2232a9
APARNA-121 <aparnasreekumar121@gmail.com>
2026-05-06 23:21:36 +0530

    initial commit

---
+++ controller/userController.js
@@ -0,0 +1,12 @@
+//register api request
+exports.registerController = (req,res)=>{
+    console.log("Inside registerController");
+    const {username,email,password} = req.body
+    console.log(username,email,password);
+    
+    res.status(200).json("Request Received")
+    
+}
+//login api
+//usereditprofile
+//admineditprofile
